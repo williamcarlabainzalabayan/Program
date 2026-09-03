446b33710f68ea592e05dd56b2da522532bbafee
WCL_DEV <williamcarlabainzalabayan@gmail.com>
2026-09-03 21:57:23 +0800

    Add initial do-while loop implementation and launch configuration

---
+++ Learning csharp in 90days/Month 1 Foundation/week 1 -  Practice 1/do-whileLoop.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace practice
+{
+    class doWhileLoop
+    {
+        static void Main()
+        {
+            bool userinfo = false;
+
+        do
+        {
+            Console.WriteLine("This WILL print once!");
+                } while (userinfo);
+                Console.WriteLine("Program done.");
+                }
+            }
+        }
