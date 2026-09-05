e1002010c37896f64938a9454cd0eef07280d24a
WCL_DEV <williamcarlabainzalabayan@gmail.com>
2026-09-05 14:12:28 +0800

    Refactor ForLoop program to implement a grid drawing feature with user-defined dimensions

--- Learning csharp in 90days/Month 1 Foundation/week 2/ForLoop.cs
+++ Learning csharp in 90days/Month 1 Foundation/week 2/ForLoop.cs
@@ -6,7 +6,7 @@ namespace forLoop
     {
         static void Main(string[] args)
         {
-
+            /*
             Console.Write("Number of integration: ");
             int number = Convert.ToInt32(Console.ReadLine());
 
@@ -14,6 +14,23 @@ namespace forLoop
             {
                 Console.WriteLine("The value of i is: " + i);
             }
+          */
+
+         Console.Write("Horizontal line: ");
+         int horizontalLine = Convert.ToInt32(Console.ReadLine());
+         Console.Write("Vertical line: ");
+         int verticalLine = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < verticalLine; i++)
+                {
+                    for (int j = 0; j < horizontalLine; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+       
+
         }
     }
 }
